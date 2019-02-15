e64547bff583ad478c8fe14cde4d8814174f090e
Divyansha <divyansha1115@gmail.com>
2019-02-15 18:42:14 +0530

    Matrix factorisation and user based

---
+++ heroku2/app.py
@@ -0,0 +1,184 @@
+from flask import Flask, render_template,request,url_for
+from math import sqrt
+import pandas as pd
+from pymongo import MongoClient
+import csv
+app = Flask(__name__)
+import random
+import numpy as np
+from scipy.sparse.linalg import svds
+
+client = MongoClient("mongodb+srv://divyansha1115:papamumma1115@cluster0-znlly.mongodb.net/test?retryWrites=true")
+db = client.app
+collection1 = db.moviesf
+collection2 = db.imdbf
+
+
+@app.route('/')
+def home():	
+	def get_random_doc():
+		count = collection2.count()
+		return collection2.find()[random.randrange(count)]
+	title = []
+	for i in range(1,50):
+		name = get_random_doc()
+		title.append(name)
+		
+	return render_template('home3.html' , title = title)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	
+	userId = 610
+	if request.method == 'POST':
+		userId = userId+1
+		result = request.form.to_dict()
+		movieId = []
+		ratings = []
+		for x in result:
+			movieId.append(int(x))
+			ratings.append(result[x])
+		#find numbber of non zero elements
+		# count = 0
+		# for i in ratings:
+		# 	if i!= '':
+		# 		count=count+1
+		#replace empty with zero and convert rest to int
+		a=0;
+		for i in ratings:
+			if i == '':
+				ratings[a]=0
+			else:
+				ratings[a] = int(i)
+
+			a=a+1
+		#calculate avg of number of rated
+		# avg = sum(ratings)/count
+		# a=0;
+		# #replace the 0 ones with avg
+		# for i in ratings:
+		# 	if i == 0:
+		# 		ratings[a]=avg
+		# 	a=a+1
+
+		
+	
+
+		# with open('ml-latest-small/ratings.csv.csv', 'a') as newFile:
+		#     newFileWriter = csv.writer(newFile)
+		#     for i in range(len(movieId)):
+		#     	newFileWriter.writerow([userId,movieId[i],ratings[i],'not_def'])
+
+		df2 = pd.read_csv("ml-latest-small/PREIIITDmovies.csv")
+		df3 = pd.read_csv("ml-latest-small/PreIIITDratings.csv")
+		df3 = df3.drop(df3.columns[0], axis=1)
+		df2 = df2.drop(df2.columns[0], axis=1)
+
+		for i in range(len(movieId)):
+			l = [userId,movieId[i],ratings[i],0]
+			df5 = pd.DataFrame([l],columns=['userId','movieId','rating','timestamp'])
+			
+			df3= pd.concat([df3,df5])
+			df3 = df3.reset_index(drop=True)
+		
+					
+
+		def pearson_correlation(userId1,userId2):
+
+		    # To get both rated movies
+		    both_rated = {}
+		    for movie in user_movie[userId1]:
+		        if movie in user_movie[userId2]:
+		            both_rated[movie] = 1
+
+		    number_of_ratings = len(both_rated)		
+		    
+		    # Checking for number of ratings in common
+		    if number_of_ratings == 0:
+		        return 0
+
+		    # Add up all the preferences of each user
+		    userId1_preferences_sum = sum([user_movie[userId1][movie] for movie in both_rated])
+		    userId2_preferences_sum = sum([user_movie[userId2][movie] for movie in both_rated])
+
+		    # Sum up the squares of preferences of each user
+		    userId1_square_preferences_sum = sum([pow(user_movie[userId1][movie],2) for movie in both_rated])
+		    userId2_square_preferences_sum = sum([pow(user_movie[userId2][movie],2) for movie in both_rated])
+
+		    # Sum up the product value of both preferences for each movie
+		    product_sum_of_both_users = sum([user_movie[userId1][movie] * user_movie[userId2][movie] for movie in both_rated])
+
+		    # Calculate the pearson score
+		    numerator = product_sum_of_both_users - (userId1_preferences_sum*userId2_preferences_sum/number_of_ratings)
+		    denominator = sqrt((userId1_square_preferences_sum - pow(userId1_preferences_sum,2)/number_of_ratings) * (userId2_square_preferences_sum -pow(userId2_preferences_sum,2)/number_of_ratings))
+		    if denominator == 0:
+		        return 0
+		    else:
+		        r = numerator/denominator
+		        return r 
+
+
+	
+	    
+		def user_reommendations(user):
+
+		    # Gets recommendations for a user by using a weighted average of every other user's rankings
+		    totals = {}
+		    simSums = {}
+		    rankings_list =[]
+		    for other in user_movie:
+		        # don't compare me to myself
+		        if other == user:
+		            continue
+		        similarity = pearson_correlation(user,other)
+		        
+
+		        # ignore scores of zero or lower
+		        if similarity <=0: 
+		            continue
+		        for movie in user_movie[other]:
+
+		            # only score movies i haven't seen yet
+		            if movie not in user_movie[user] or user_movie[user][movie] == 0:
+
+		            # Similrity * score
+		                totals.setdefault(movie,0)
+		                totals[movie] += user_movie[other][movie]* similarity
+		                # sum of similarities
+		                simSums.setdefault(movie,0)
+		                simSums[movie]+= similarity
+
+		        # Create the normalized list
+
+		    rankings = [(total/simSums[movie],movie) for movie,total in totals.items()]
+		    rankings.sort()
+		    rankings.reverse()
+		    # returns the recommended movies
+		    recommendataions_list = [recommend_movie for score,recommend_movie in rankings]
+		    return recommendataions_list[:10] 
+
+		df = pd.merge(df3, df2, on='movieId')
+		df1 = df[['userId','title','rating']]
+		dict1 =df1.groupby('userId')[['title','rating']].apply(lambda g: g.values.tolist()).to_dict()
+		for key in dict1:
+			dict1[key]=dict(dict1[key])
+		user_movie = dict1
+		result = predictions = user_reommendations(userId)
+
+		title = []
+		def doc_info(obj):
+			for y in obj:
+				return dict(y)
+		for x in result:
+			myquery = { "Title": x }
+			mydoc = collection2.find(myquery)
+			title.append(doc_info(mydoc))
+
+
+		return render_template("result3.html",title = title )
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ heroku3/app.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template,request,url_for
+import pandas as pd
+from pymongo import MongoClient
+app = Flask(__name__)
+import random
+
+
+client = MongoClient("mongodb+srv://divyansha1115:papamumma1115@cluster0-znlly.mongodb.net/test?retryWrites=true")
+db = client.app
+collection1 = db.moviesf
+collection2 = db.imdbf
+
+
+
+
+@app.route('/')
+def home():	
+	def get_random_doc():
+		count = collection2.count()
+		return collection2.find()[random.randrange(count)]
+	title = []
+	for i in range(1,10):
+		name = get_random_doc()
+		title.append(name)
+
+		
+	return render_template('home.html' , title = title)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	df2 = pd.read_csv("ml-latest-small/PREIIITDmovies.csv")
+	df3 = pd.read_csv("ml-latest-small/PreIIITDratings.csv")
+	df3 = df3.drop(df3.columns[0], axis=1)
+	df2 = df2.drop(df2.columns[0], axis=1)
+	# find the mean of each user
+	if request.method == 'POST':
+		user_group = df3.groupby(by='userId')
+		user_means = user_group['rating'].agg(['mean', 'count'])
+		# create a new column named "meanCenteredRating"
+		# this function takes in ratings of one user and return mean_centered ratings of that user
+		mean_centering = lambda ratings: ratings - ratings.mean()
+		df3['meanCenteredRating'] = user_group['rating'].transform(mean_centering)
+		df_mean = pd.merge(df3, df2, on='movieId')
+		def recommend_user(user_rating,movie_matrix):
+		    corrMatrix = movie_matrix.corr(method='pearson', min_periods=10)
+		    similar_candidates = pd.Series()
+		    for i in range(0, len(user_rating.index)):
+		        # retrieve similar movies to this one that I rated
+		        similar_movies = corrMatrix[user_rating.index[i]].dropna()
+		        # scale its similarity by how well I rated this movie
+		        similar_movies = similar_movies.map(lambda x: x * user_rating[i])
+		        # add the score to the list of similar candidates
+		        similar_candidates = similar_candidates.append(similar_movies)
+		    similar_candidates.sort_values(inplace = True, ascending = False)
+		    similar_candidates = similar_candidates.groupby(similar_candidates.index).sum()
+		    similar_candidates.sort_values(inplace = True, ascending = False)
+		    return similar_candidates.head(10)
+
+
+		movie_matrix_normalazied = df_mean.pivot_table(index='userId', columns='title', values='meanCenteredRating')
+		#retrieve ratings and normalized them and convert them into pandas series
+		result = request.form.to_dict()
+		title = []
+		ratings = []
+		for x in result:
+			title.append(x[:-1])
+			ratings.append(result[x])
+		a=0
+		count = 0
+		#find numbber of non zero elements
+		for i in ratings:
+			if i!= '':
+				count=count+1
+
+		#replace empty with zero and convert rest to int
+		for i in ratings:
+			if i == '':
+				ratings[a]=0
+			else:
+				ratings[a] = int(i)
+
+			a=a+1
+		#calculate avg of number of rated
+		avg = sum(ratings)/count
+		a=0;
+		#normalise all the ratings and replace the 0 ones with avg
+		for i in ratings:
+			ratings[a]=avg - ratings[a]
+			a=a+1
+
+		# return render_template("result2.html",title = title,ratings=ratings)
+
+		df=pd.DataFrame(ratings,index=title)
+		series = df.iloc[:,0]
+		myRatings_normalazied = series
+		#return render_template("result2.html",title = myRatings_normalazied)
+
+		ans = recommend_user(myRatings_normalazied,movie_matrix_normalazied)
+		#ratings = ans.tolist()	
+		result = ans.index.tolist()
+		title = []
+		def doc_info(obj):
+			for y in obj:
+				return dict(y)
+		for x in result:
+			myquery = { "Title": x }
+			mydoc = collection2.find(myquery)
+			title.append(doc_info(mydoc))
+			
+		return render_template("result3.html",title = title )
+
+
+		#end code
+		
+		
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+
+
+
+
+
+
+
+
